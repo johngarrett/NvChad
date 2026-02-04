60f9a17dd18a1b2a65f8750a1b80d53f982fbe42
garrepi <jacgarrett18@gmail.com>
2026-02-04 10:58:49 -0800

    find typescript files

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,16 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("n", "<leader>ft", function()
+  require("telescope.builtin").find_files {
+      prompt_title = "TypeScript Files",
+      find_command = {
+        "rg",
+        "--files",
+        "--glob",
+        "*.ts",
+        "--glob",
+        "*.tsx",
+      },
+  }
+end, { desc = "Find TypeScript files" })

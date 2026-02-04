5fa09e96353a3ac2aa5ee1d46da8c788f6944b73
garrepi <jacgarrett18@gmail.com>
2026-02-04 11:25:22 -0800

    add formatters for ts and js

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,6 +1,8 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
+    javascript = { "prettier" },
+    typescript = { "prettier" },
     -- css = { "prettier" },
     -- html = { "prettier" },
   },
